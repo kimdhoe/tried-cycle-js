aa3708714bdd9525c7334ca7814158ed60ace302
kimdhoe <kimdhoe1@gmail.com>
2016-08-10 08:13:07 +0900

    Seconds elapsed (with reset feature)

---
+++ index.js
@@ -0,0 +1,31 @@
+import xs from 'xstream'
+import { run } from '@cycle/xstream-run'
+import { div, p, makeDOMDriver } from '@cycle/dom'
+
+const intent = sources =>
+  sources.events('click')
+
+const model = reset$ =>
+  reset$
+    .startWith(null)
+    .map(() => xs.periodic(1000))
+    .flatten()
+
+const main = sources => {
+  const reset$  = intent(sources.DOM.select('.seconds'))
+  const second$ = model(reset$)
+  const vdom$   = second$
+                    .map( n => div( '.seconds'
+                                  , [ p(`Seconds elapsed: ${n}`) ]
+                                  )
+                        )
+
+
+  const sinks = { DOM: vdom$ }
+
+  return sinks
+}
+
+const drivers = { DOM: makeDOMDriver('#app') }
+
+run(main, drivers)
