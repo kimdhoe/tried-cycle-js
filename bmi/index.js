cb15bb3c14c5ccec64a5f398ea0abc32ee84ecfa
kimdhoe <kimdhoe1@gmail.com>
2016-08-10 13:12:33 +0900

    Refactor BMI

--- bmi/index.js
+++ bmi/index.js
@@ -22,21 +22,25 @@ function model (weight$, height$) {
     .map(([weight, height]) => ({bmi: bmi(weight, height), weight, height}))
 }
 
+function renderWeightSlider(weight) {
+  return div([
+    p(`Weight: ${weight}kg`),
+    input('.weight', {attrs: {type: 'range', min: 40, max: 150, value: weight}})
+  ])
+}
+
+function renderHeightSlider(height) {
+  return div([
+    p(`Height: ${height}cm`),
+    input('.height', {attrs: {type: 'range', min: 140, max: 220, value: height}})
+  ])
+}
+
 function view (world$) {
   return world$.map(({bmi, weight, height}) =>
     div([
-      div([
-        p(`Weight: ${weight}kg`),
-        input('.weight', {
-          attrs: {type: 'range', min: 40, max: 150, value: weight}
-        })
-      ]),
-      div([
-        p(`Height: ${height}cm`),
-        input('.height', {
-          attrs: {type: 'range', min: 140, max: 220, value: height}
-        })
-      ]),
+      renderWeightSlider(weight),
+      renderHeightSlider(height),
       h2(`BMI is ${bmi}.`)
     ])
   )
